1f4eb0d81e0947d7108683168a4ade283fcf4ebd
Victor Pavageau <victor.pavageau@viacesi.fr>
2024-02-03 16:50:05 +0100

    feat: mock the landing page main elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Navbar from './components/Navbar';
+
 function App(): JSX.Element {
+  const logo = 'src/assets/logo.png';
+
   return (
-    <div className="h-screen w-screen global-background">
-      Main page
+    <div className="h-screen w-screen overview-background">
+      <Navbar />
+      <div className="flex justify-between lg:mt-20">
+        <h1 className="lg:ml-32 lg:max-w-md leading-relaxed text-[#334225]">
+          Vert un monde plus responsable
+        </h1>
+        <img className="lg:mr-32 lg:max-w-md leading-relaxed text-[#334225] rounded-xl" src={logo} alt="Illustration de l'évennement" />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
 function Navbar(): JSX.Element {
+  const logo = 'src/assets/logo.png';
+
   return (
-    <div>
-      Navbar
+    <div className="w-screen h-20 text-black">
+      <img className="h-16 w-auto float-left ml-5 mt-2" src={logo} alt="Vert un monde responsable logo" />
+      <div className="h-full flex justify-between align-middle items-center lg:mx-80">
+        <div>
+          L'évennement
+        </div>
+        <div>
+          Le lieu
+        </div>
+        <div>
+          Les ateliers
+        </div>
+        <div>
+          Contact
+        </div>
+      </div>
     </div>
   );
 }
