7024aa9287bed18fa0897d38cb48aff28c7f1843
Victor Pavageau <victor.pavageau@viacesi.fr>
2024-02-05 14:10:18 +0100

    fix: fix map url

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,7 +7,7 @@ function Map(): JSX.Element {
         </h2>
       </div>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d727.8519078909577!2d-0.560802983108519!3d44.849366757590204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527d6c96fcb75%3A0x2cf11b39e4213f57!2sEcocycle%20Darwin!5e0!3m2!1sfr!2sfr!4v1707137631071!5m2!1sfr!2sfr"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d707.1566811383864!2d-0.5622191345273478!3d44.849534628333735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd55278f7241363f%3A0x5cc17e265bd8fae7!2sSHOP%20SAUVAGES%20-%20Organic%20Dealers%20-%20DARWIN!5e0!3m2!1sfr!2sfr!4v1707138399988!5m2!1sfr!2sfr"
         width="600"
         height="450"
         className="border-0 border-none w-full mt-10"
