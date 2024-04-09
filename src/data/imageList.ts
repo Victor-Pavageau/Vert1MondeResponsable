d6cf975526ac8c1298f233e1b71fd7478c86ee99
Victor Pavageau <victor.pavageau@viacesi.fr>
2024-04-09 09:49:26 +0200

    release: v1.2.2
    * chore: add workshop image files
    
    * feat: replace fake workshop images and descriptions by real data
    
    * feat: add facebook and linkedin contact informations
    
    * chore: improve SEO score
    
    * fix: add redirect link on logo click
    
    * feat: add event presentation instead of fake text
    
    * style: adding emojis in event and workshops presentations
    
    * style: fix responsive issues
    
    * feat: add credit
    
    * refactor: global refactor and avoid css duplication
    
    * fix: update event endtime
    
    * style: improve responsive credit component
    
    * chore: add vercel analytics
    
    * feat: add workshop 4 activity page
    
    * fix: fix redirect issues on vercel host
    
    * fix: improve images description

--- src/data/imageList.ts
+++ src/data/imageList.ts
@@ -2,50 +2,48 @@ import { Image } from '../types';
 
 export const imageList: Image[] = [
   {
-    src: './assets/workshop4/image1.webp',
+    src: './assets/workshop4/image1.png',
     goodAlt: `
-Dans une cuisine chaleureuse et bien éclairée, une femme âgée, les cheveux gris et le sourire radieux, est en plein travail de pâtisserie. Ses lunettes rondes se posent sur le bout de son nez, et son visage exprime la joie et la satisfaction. Elle porte un tablier rayé sur sa blouse fleurie, ajoutant une touche de charme à son apparence soignée.
-Devant elle, sur le plan de travail en bois, s'étend une pâte qu'elle étale consciencieusement avec un rouleau à pâtisserie. À proximité, des croissants déjà formés attendent d'être cuits, disposés avec ordre sur une plaque de cuisson. La texture feuilletée des croissants est visible, suggérant leur futur croustillant doré une fois sortis du four. Autour d'elle, la cuisine déborde d'ustensiles de cuisine utilisés dans la préparation, tandis que le décor rustique avec ses pots d'herbes et son atmosphère accueillante fait penser aux cuisines d'antan, emplies d'histoires et de traditions culinaires. C'est un moment de création culinaire capturé dans une ambiance qui évoque chaleur, famille et la simplicité des plaisirs de la vie.
+L'image saisit un moment décisif de la finale de la Coupe du Monde de Football de 1998 entre la France et le Brésil. Un joueur français en bleu, numéro 18, se détache en effectuant un saut impressionnant pour reprendre de la tête le ballon, symbolisant la détermination et l'esprit de compétition. Juste en dessous, un Brésilien, également numéro 18, tente de le contrer. Autour, d'autres joueurs sont attentifs à l'action, notamment un célèbre Brésilien au fond, reconnaissable à sa coupe de cheveux caractéristique et portant le numéro 6. Cette scène illustre l'intensité et la passion du football à son plus haut niveau, rappelant un match entré dans la légende.
     `,
     badAlt: `
-Une femme aux cheveux gris et au sourire content est en train de travailler de la pâte dans une cuisine lumineuse, avec plusieurs formes arrondies de pâte disposées devant elle sur le comptoir.
+Dans cette image, on observe plusieurs sportifs.
     `,
   },
   {
-    src: './assets/workshop4/image2.png',
+    src: './assets/workshop4/image2.webp',
     goodAlt: `
-Dans cette scène, nous voyons un moment crucial d'un match de football très attendu, celui de la France contre le Brésil lors de la Coupe du Monde de 1998. Le stade est rempli à ras bord, une mer de spectateurs aux visages flous en arrière-plan, centrée sur l'action palpitante au premier plan. Deux équipes s'affrontent avec vigueur: les Français, vêtus de bleu, luttent pour le contrôle du ballon contre les Brésiliens en jaune et bleu.
-Au centre de l'image, un joueur français en bleu, avec des bandes rouges sur ses chaussettes et le numéro 18, est capturé en plein saut athlétique, la tête tendue vers le haut pour frapper le ballon blanc en approche. Ses cuisses sont parallèles au sol, dénotant un saut puissant et maîtrisé. Juste en dessous de lui, un adversaire brésilien, portant le même numéro, semble tenter désespérément de contester le ballon, les cheveux ébouriffés par le mouvement et le visage concentré sur la tâche à accomplir.
-Autour d'eux, d'autres joueurs suivent l'action de près, certains en mouvement, d'autres regardant, témoins de la bataille aérienne. Le joueur français au premier plan, également en bleu, regarde son coéquipier s'envoler avec espoir et anticipation. À l'arrière-plan, un joueur brésilien notoire, identifiable par son unique coupe de cheveux et le numéro 6, observe la scène, probablement en évaluant la situation et en préparant sa prochaine action.
-L'intensité du match est palpable, chaque joueur capté dans cette image représentant une pièce vitale du dynamisme et de la stratégie en jeu dans ce qui est considéré comme l'un des affrontements les plus mémorables de l'histoire du football.
+Une femme senior, les cheveux argentés et souriante, prépare des pâtisseries dans une cuisine lumineuse. Avec ses lunettes sur le nez et un tablier rayé, elle roule consciencieusement de la pâte à côté de croissants prêts à être cuits. L'atmosphère chaleureuse de la cuisine évoque traditions et convivialité familiale.
     `,
     badAlt: `
-Dans cette image, on assiste à une scène de compétition sportive où deux équipes de joueurs en tenue de sport s'affrontent pour la possession d'un ballon. Les joueurs de l'équipe en bleu s'efforcent d'intercepter le ballon en l'air, tandis qu'un membre de l'équipe vêtue de jaune et bleu tente également de le jouer. L'action se déroule dans un stade rempli de spectateurs, témoignant de l'ambiance enthousiaste qui y règne. L'énergie du jeu est palpable, avec les joueurs concentrés et dynamiques dans leur mouvement.    `,
+Une personne dans une cuisine, prépare un plat.
+    `,
   },
   {
     src: './assets/workshop4/image3.jpg',
     goodAlt: `
-Le tableau dépeint la Joconde, également connue sous le nom de Mona Lisa, l'une des œuvres d'art les plus célèbres et les plus étudiées au monde. Elle est assise contre un fond flou qui s'étend dans un paysage lointain de montagnes et d'eau, suggérant la profondeur et l'éloignement. La lumière douce du tableau donne à la peau de la Joconde un éclat lumineux et lisse, et l'artiste a adroitement utilisé la technique du sfumato pour adoucir les transitions entre les ombres et les lumières sur son visage, créant ainsi son sourire insaisissable.
-Elle est vêtue d'une robe de l'époque de la Renaissance avec des manches bouffantes et un décolleté en V sombre, qui contraste avec son teint clair. Ses mains sont posées l'une sur l'autre d'une manière décontractée mais élégante, ce qui ajoute à la tranquillité générale du portrait. Sa coiffure et ses vêtements reflètent le style de la haute société italienne de l'époque. Derrière elle, le paysage rêveur et la composition harmonieuse contribuent à l'atmosphère paisible et intemporelle du tableau. Peint par Leonardo da Vinci, un maître de la Renaissance, la Joconde est célèbre pour son réalisme innovant, son charme mystérieux et son sourire énigmatique, capturant l'imagination des spectateurs pendant des siècles.    `,
+La Joconde, œuvre emblématique de Léonard de Vinci, illustre une femme assise, son sourire subtil rehaussé par la technique du sfumato qui fusionne les ombres et la lumière. Elle porte une robe renaissante et est placée devant un paysage flou, soulignant son mystère et l'élégance de son époque. On a l'impression que la joconde nous regarde.
+    `,
     badAlt: `
-L'image présente le portrait d'une femme au regard calme et au sourire subtil. Elle est vêtue d'une robe aux teintes terreuses, avec des manches plissées. Derrière elle, un paysage flou de montagnes et de voies d'eau se fond dans un horizon brumeux.    `,
+Cette image représente un tableau, avec une personne.
+    `,
   },
   {
     src: './assets/workshop4/image4.jpg',
     goodAlt: `
-L'image illustre un moment tendre et paisible dans la vie de deux koalas. Le plus grand des deux, probablement l'adulte, est assis sur une branche robuste, son dos contre le tronc d'un arbre rugueux. Sa fourrure est d'un gris doux, dense et floue, ce qui lui donne une apparence duveteuse. Les oreilles sont grandes, arrondies et pelucheuses, avec des poils blancs contrastant avec le gris plus foncé à l'intérieur. Les yeux sont petits, noirs, vifs et semblent exprimer une tranquillité sereine.
-Le plus petit koala, sans doute le jeune, est blotti contre le flanc de l'adulte dans une étreinte confiante, cherchant confort et sécurité. Ses yeux sont fermés, et son expression est celle du contentement. La pose du jeune évoque une innocence juvénile et une dépendance touchante envers son parent.
-Leur habitat naturel, qui peut être discerné en arrière-plan, est composé de branches d'arbres et de feuillage vert, créant une canopée qui leur offre protection et nourriture. La lumière diffuse crée une ambiance douce et tamisée à travers la scène, soulignant la tranquillité et la simplicité de ce moment de la vie sauvage. C'est une image qui évoque non seulement l'aspect adorable de ces créatures mais aussi leur relation étroite et leur adaptation à l'environnement forestier.
+L'image décrit un instant doux entre un koala adulte et son petit, blotti contre lui, dans leur habitat naturel parsemé de branches et de feuillage. La lumière douce met en valeur cette scène paisible et la complicité qui unit les deux animaux.
     `,
     badAlt: `
-L'image montre deux animaux au pelage gris doux en train de se blottir l'un contre l'autre sur une épaisse branche d'arbre. L'animal plus grand, avec une fourrure particulièrement pelucheuse et de grandes oreilles arrondies, s'appuie contre le tronc de l'arbre, ses yeux attentifs et apaisés regardant au loin. À ses côtés, un plus petit, avec des traits doux et une expression paisible, s'enroule confortablement contre le grand, les yeux mi-clos en signe de contentement. Les deux semblent partager un moment de calme et d'affection. Ils sont entourés par un cadre naturel de branches entrelacées et de feuillage, qui évoque une atmosphère sereine et isolée.    `,
+Cette photographie, montre plusieurs animaux sur du bois.
+    `,
   },
   {
     src: './assets/workshop4/image5.jpg',
     goodAlt: `
-L'image capture un homme en action, agenouillé près d'une voiture bleue ciel, concentré sur la roue avant du côté conducteur. Il porte des vêtements de travail décontractés, un sweat-shirt bleu foncé et un jean, avec des gants rouges qui offrent un contraste vif. Ses outils, y compris une perceuse électrique sans fil, sont posés sur le sol à portée de main, prêts à être utilisés. Le véhicule est levé légèrement du sol, probablement sur un cric, indiquant qu'un entretien ou une réparation est en cours. L'environnement semble être un atelier de mécanique, avec un sol industriel et d'autres équipements en arrière-plan.
+L'image capture un homme en action, agenouillé près d'une voiture bleue ciel, concentré sur la roue avant du côté conducteur. Il porte des vêtements de travail décontractés. Ses outils sont posés sur le sol à portée de main, prêts à être utilisés. Le mecanicien effectue le changement de la roue.
     `,
     badAlt: `
-Une personne est accroupie au sol, travaillant attentivement sur un objet circulaire relié à une structure plus grande. Des outils sont éparpillés à proximité sur le sol.    `,
+Cette photographie met en avant une voiture.
+    `,
   },
 ];
